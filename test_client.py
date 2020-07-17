8765a096b6c76d4a00eb0fa6dc49c5faf95c0c2f
ada-feng <46980060+ada-feng@users.noreply.github.com>
2020-07-16 19:14:33 -0600

    Update test_client.py
    changed misleading naming

--- test_client.py
+++ test_client.py
@@ -27,7 +27,7 @@ class inventory_client():
         return status_code, body_dic
 
     @staticmethod
-    def response_body_formatter(body_dic):
+    def request_body_formatter(body_dic):
         return (json.dumps(body_dic)).encode('utf-8')
 
 class TestAPIResponse(unittest.TestCase):
@@ -73,10 +73,10 @@ class TestAPIResponse(unittest.TestCase):
 
     def testGetExistItem(self):
         setup_dic = {"a": "6"}
-        setup_dic_formatted = self.iv_client.response_body_formatter(setup_dic)
+        setup_dic_formatted = self.iv_client.request_body_formatter(setup_dic)
         self.iv_client.httpmethod("PUT", self.path, setup_dic_formatted)
         request_body_dic = {'name': 'a'}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body  =  self.iv_client.httpmethod('get', self.path, \
         request_body_formatted)
         self.assertEqual(status, 200 )
@@ -87,28 +87,28 @@ class TestAPIResponse(unittest.TestCase):
     def testGetNotExistItem(self):
         #first put 'a' in the inventory
         setup_dic = {"a": "6"}
-        setup_dic_formatted = self.iv_client.response_body_formatter(setup_dic)
+        setup_dic_formatted = self.iv_client.request_body_formatter(setup_dic)
         status, response_body = self.iv_client.httpmethod("PUT", self.path, setup_dic_formatted)
         self.assertEqual(status, 200 )
         self.assertTrue( response_body == {'a':6})
         #now delete "a", so it cannot be in the inventory
         request_body_dic = {'name': 'a'}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body = self.iv_client.httpmethod('DELETE', self.path,request_body_formatted)
         self.assertEqual(status, 200 )
         self.assertEqual( response_body, {})
         #now query th string
         request_body_dic = {"name": "a"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         with self.assertRaises(Exception):
             self.iv_client.httpmethod('get', self.path, request_body_formatted)
 
     def testGetOnlyUpperBound(self):
         setup_dic = {"a": "25", "b":6, "c":"26"}
-        setup_dic_formatted = self.iv_client.response_body_formatter(setup_dic)
+        setup_dic_formatted = self.iv_client.request_body_formatter(setup_dic)
         self.iv_client.httpmethod("PUT", self.path, setup_dic_formatted)
         request_body_dic = {'upper bound': '25'}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body  =  self.iv_client.httpmethod('get', self.path, \
         request_body_formatted)
         self.assertEqual(status, 200 )
@@ -118,10 +118,10 @@ class TestAPIResponse(unittest.TestCase):
 
     def testGetOnlyLowerBound(self):
         setup_dic = {"a": "25", "b":6, "c":"7"}
-        setup_dic_formatted = self.iv_client.response_body_formatter(setup_dic)
+        setup_dic_formatted = self.iv_client.request_body_formatter(setup_dic)
         self.iv_client.httpmethod("PUT", self.path, setup_dic_formatted)
         request_body_dic = {'lower bound': '7'}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body  =  self.iv_client.httpmethod('get', self.path,\
          request_body_formatted)
         self.assertEqual(status, 200 )
@@ -131,10 +131,10 @@ class TestAPIResponse(unittest.TestCase):
 
     def testGetBothBounds(self):
         setup_dic = {"a": "17", "b":16, "c":"300", 'd':301}
-        setup_dic_formatted = self.iv_client.response_body_formatter(setup_dic)
+        setup_dic_formatted = self.iv_client.request_body_formatter(setup_dic)
         self.iv_client.httpmethod("PUT", self.path, setup_dic_formatted)
         request_body_dic = {'lower bound': '17', 'upper bound': 300}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body  =  self.iv_client.httpmethod('get', self.path, \
         request_body_formatted)
         self.assertEqual(status, 200 )
@@ -146,7 +146,7 @@ class TestAPIResponse(unittest.TestCase):
     def testGetIllegalBounds(self):
         lowerHigherThanUpper_dic = {'lower bound': '17', 'upper bound': 3}
         lowerHigherThanUpper_dic_formatted \
-        = self.iv_client.response_body_formatter(lowerHigherThanUpper_dic)
+        = self.iv_client.request_body_formatter(lowerHigherThanUpper_dic)
         status, response_body  =  self.iv_client.httpmethod('get', self.path, \
         lowerHigherThanUpper_dic_formatted)
         self.assertEqual(status, 200 )
@@ -154,14 +154,14 @@ class TestAPIResponse(unittest.TestCase):
 
         notInteger_dic = {'lower bound': '1.7', 'upper bound': "hello"}
         notInteger_dic_formatted \
-        = self.iv_client.response_body_formatter(notInteger_dic)
+        = self.iv_client.request_body_formatter(notInteger_dic)
         with self.assertRaises(Exception):
             self.iv_client.httpmethod('get', self.path, notInteger_dic_formatted)
 
     def testPutNormal(self):
         #a normal dictionary with names of integer/strings of integer values
         request_body_dic = {"a": 5, "b": "6", "c":85}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body = self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
         self.assertEqual(status, 200 )
         self.assertEqual(response_body, {"a": 5, "b": 6, "c":85} )
@@ -169,18 +169,18 @@ class TestAPIResponse(unittest.TestCase):
     def testPutNegative(self):
         #a normal dictionary with names of integer/strings of integer values
         request_body_dic = {"a": 5, "b": -6}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         with self.assertRaises(Exception):
             self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
 
     def testPutIncDec(self):
         #set up
         request_body_dic = {"a": 5, "b": "6", "c":85}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
         #increase or decrease
         request_body_dic = {"a": "-3", "b": "+2", "c": +60}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body = self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
         self.assertEqual(status, 200 )
         self.assertEqual(response_body, {"a": 2, "b": 8, "c":+60} )
@@ -188,12 +188,12 @@ class TestAPIResponse(unittest.TestCase):
     def testPutIncDecResultNegative(self):
         #set up
         request_body_dic = {"a": 5, "b": "6"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
 
         #increase or decrease
         request_body_dic = {"a": "-7", "b": "+2"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         with self.assertRaises(Exception):
             self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
 
@@ -201,18 +201,18 @@ class TestAPIResponse(unittest.TestCase):
     def testPutWeirdStringAndAtomic(self):
         #set up
         request_body_dic = {"a": "7", "b": "2"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
 
         #increase or decrease
         request_body_dic = {"a": "6.05", "b": "6"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         with self.assertRaises(Exception):
             self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
 
         #test if b is changed
         request_body_dic = {"name": "b"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         status, response_body = self.iv_client.httpmethod("get", self.path, request_body_formatted)
         self.assertEqual(response_body["b"], 2)
 
@@ -220,12 +220,12 @@ class TestAPIResponse(unittest.TestCase):
     def testDeleteNonExist(self):
         #set up
         request_body_dic = {"a": "2"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         self.iv_client.httpmethod("PUT", self.path, request_body_formatted)
 
         #delete the item a
         request_body_dic = {"name": "a"}
-        request_body_formatted = self.iv_client.response_body_formatter(request_body_dic)
+        request_body_formatted = self.iv_client.request_body_formatter(request_body_dic)
         self.iv_client.httpmethod("DELETE", self.path, request_body_formatted)
 
         #now we shouldn't be able to do it again
